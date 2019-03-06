1975c285af90981666a9dddfc29f13dbf101971b
sannevm <sannevanmourik@hotmail.com>
2019-03-06 11:48:20 +0100

    adds serializers.py

---
+++ src/todo/serializers.py
@@ -0,0 +1,8 @@
+form rest_framework import serializers
+from todo.models import TodoItem
+
+class TodoItemSerializer(serializers.HyperlinkedModelSerializer):
+    url = serializers.ReadOnlyField()
+    class Meta:
+        model = TodoItem
+        fields = ('url', 'title', 'completed', 'order')
